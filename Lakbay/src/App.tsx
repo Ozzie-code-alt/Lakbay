134c6d26a06c16eed4086b6d962442a52fea0741
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-10 22:42:51 +0800

    Updates

--- Lakbay/src/App.tsx
+++ Lakbay/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <>
@@ -10,7 +10,6 @@ function App() {
             <div className="lakbay-logo-container">
               <div className="lakbay-logo-img-container">
                 <img src="" alt="" />
-
               </div>
               <h1 className="Lakbay-text"> LAKBAY</h1>
             </div>
@@ -27,12 +26,16 @@ function App() {
           </nav>
 
           <div className="main-page-container">
-                <video autoPlay loop muted > 
-                <source src="/src/assets/background.mp4" type="video/mp4" ></source></video>
-            <div className="page-description-container">           
+            <video autoPlay loop muted>
+              <source
+                src="/src/assets/background.mp4"
+                type="video/mp4"
+              ></source>
+            </video>
+            <div className="page-description-container">
               <div className="title-container">
                 <h1>
-                   WELCOME  TO <br />  <span>Philippines</span>
+                  WELCOME TO <br /> <span>Philippines</span>
                 </h1>
               </div>
               <div className="img-container">
@@ -40,14 +43,20 @@ function App() {
               </div>
               <div className="phrase-container">
                 <p>
-                  Embark on a journey like no other through pristine<br /> 
+                  Embark on a journey like no other through pristine
+                  <br />
                   beaches, majestic mountains, and vibrant cultural <br />{" "}
                   heritage
                 </p>
               </div>
 
               <div className="feature-tours-container">
-                <h1>Featured <span className="tours">Tours</span> <span className="font"><FontAwesomeIcon icon={faArrowRight}/> </span>     </h1>
+                <h1>
+                  Featured <span className="tours">Tours</span>{" "}
+                  <span className="font">
+                    <FontAwesomeIcon icon={faArrowRight} />{" "}
+                  </span>{" "}
+                </h1>
               </div>
             </div>
           </div>
@@ -56,35 +65,23 @@ function App() {
         <section className="Section-2-halfPage">
           <div className="main-container ">
             <div className="upper-half-container">
-              <div className="pic-left">
-        <img src="" alt="" />
+              <div className="pic-left pic">
+                <div className="overlay"> </div>
+                <div className="text">Travel Tips & info</div>
               </div>
-            <div className="pic-middle">
-              <img src="" alt="" />
-            </div>
-            <div className="pic-right">
-              <img src="" alt="" />
-            </div>
+              <div className="pic-middle pic">
+              <div className="overlay"></div>
+              <div className="text">What to do</div>
+              </div>
+              <div className="pic-right pic">
+              <div className="overlay"></div>
+              <div className="text">Where to Go</div>
+              </div>
 
               {/*Slider portion*/}
-                <div className="slider-container">
-
-
-                </div>
-
-
-
-
-
+              <div className="slider-container"></div>
             </div>
-
-
-
-
           </div>
-
-
-
         </section>
       </div>
     </>
