8bcdf8b023f891d1db1fc41f7cf76a15a43cfd2e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-10 18:05:28 +0800

    LakbayPush

---
+++ Lakbay/src/App.tsx
@@ -0,0 +1,94 @@
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <section className="Section-1-MainPage">
+          <nav className="navbar">
+            <div className="lakbay-logo-container">
+              <div className="lakbay-logo-img-container">
+                <img src="" alt="" />
+
+              </div>
+              <h1 className="Lakbay-text"> LAKBAY</h1>
+            </div>
+            <ul className="nav-lists">
+              <li>Home</li>
+              <li>Destination</li>
+              <li>Offers</li>
+              <li>About Us</li>
+            </ul>
+
+            <div className="BookNow-container">
+              <button className="BookNow">Book Now</button>
+            </div>
+          </nav>
+
+          <div className="main-page-container">
+                <video autoPlay loop muted > 
+                <source src="/src/assets/background.mp4" type="video/mp4" ></source></video>
+            <div className="page-description-container">           
+              <div className="title-container">
+                <h1>
+                   WELCOME  TO <br />  <span>Philippines</span>
+                </h1>
+              </div>
+              <div className="img-container">
+                <img src="" alt="" />
+              </div>
+              <div className="phrase-container">
+                <p>
+                  Embark on a journey like no other through pristine<br /> 
+                  beaches, majestic mountains, and vibrant cultural <br />{" "}
+                  heritage
+                </p>
+              </div>
+
+              <div className="feature-tours-container">
+                <h1>Featured <span className="tours">Tours</span> <span className="font"><FontAwesomeIcon icon={faArrowRight}/> </span>     </h1>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="Section-2-halfPage">
+          <div className="main-container ">
+            <div className="upper-half-container">
+              <div className="pic-left">
+        <img src="" alt="" />
+              </div>
+            <div className="pic-middle">
+              <img src="" alt="" />
+            </div>
+            <div className="pic-right">
+              <img src="" alt="" />
+            </div>
+
+              {/*Slider portion*/}
+                <div className="slider-container">
+
+
+                </div>
+
+
+
+
+
+            </div>
+
+
+
+
+          </div>
+
+
+
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
